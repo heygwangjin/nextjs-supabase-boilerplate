7bd2cdf28df09682d1932a54579e4ab231b61844
heygwangjin <heygwangjin@gmail.com>
2024-06-22 18:14:14 +0900

    settings: Use shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
+import { GeistSans } from 'geist/font/sans';
+import { Inter as FontSans } from 'next/font/google';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'Next.js and Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
 };
 
 export default function RootLayout({
@@ -18,7 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+      <body
+        className={cn(
+          'bg-background text-foreground min-h-screen  font-sans antialiased',
+          fontSans.variable
+        )}
+      >
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
